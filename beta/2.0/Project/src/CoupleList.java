5101ad9bd07e77e4fc1bb52c87c1128d163cb00f
PengFCB <1159695393@qq.com>
2019-07-04 23:02:32 +0800

    Update CoupleList.java
    bug fixed

--- beta/2.0/Project/src/CoupleList.java
+++ beta/2.0/Project/src/CoupleList.java
@@ -1,4 +1,3 @@
-import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,13 +8,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 public class CoupleList {
     public Couple[] cplist;
     private Passage[] hotList;
     private Passage[] totalList;
-	private List<Couple> cplistn;
-	private List<Couple> cplisth;
-	private List<Couple> cplistt;
+    
     public CoupleList() throws IOException {
         BufferedReader br=new BufferedReader(new FileReader("json/cpdic.json"));
         BufferedReader br1=new BufferedReader(new FileReader("json/lfhot.json"));
@@ -43,60 +42,58 @@ public class CoupleList {
     		cplist[i].resetHot();
     	}
     }
-    public void sort(){
-		for(int i=0;i<cplist.length;i++){
-			for(int j=0;j<hotList.length;j++){
-				if(hotList[j].getTag().contains(cplist[i].getTag())){
-					cplist[i].addHot(hotList[j].getHotIndex()+hotList[j].getCommentNum()*10);
-				}
-			}
-		}
-		cplisth=Arrays.asList(cplist);
-		Collections.sort(cplisth);
-		for(int i=0;i<cplist.length;i++){
-			for(int j=0;j<totalList.length;j++){
-				if(totalList[j].getTag().contains(cplist[i].getTag())){
-					cplist[i].addHot(totalList[j].getHotIndex()+totalList[j].getCommentNum()*10);
-				}
-			}
-		}
-		cplistt=Arrays.asList(cplist);
-		Collections.sort(cplistt);
-		resetCpIndex();
-
-		for(int i=0;i<cplist.length;i++){
-			for(int j=0;j<totalList.length;j++){
-				if(totalList[j].getTag().contains(cplist[i].getTag())){
-					cplist[i].addHot(totalList[j].getHotIndex()+totalList[j].getCommentNum()*10);
-				}
-			}
-		}
-		cplistn=Arrays.asList(cplist);
-		Collections.sort(cplistn);
-	}
-	public void totxt()throws IOException{
-		BufferedWriter bw=new BufferedWriter(new FileWriter("txt/cpHotListRank.txt"));
-		BufferedWriter bw1=new BufferedWriter(new FileWriter("txt/cpTotListRank.txt"));
-		BufferedWriter bw2=new BufferedWriter(new FileWriter("txt/cpRank.txt"));
-		for(int i=0;i<cplisth.size();i++){
-			bw.write(cplisth.get(i).getTag()+"\t"+cplisth.get(i).getHotIndex()+"\n");
-			bw.flush();
-		}
-		bw.close();
-		for(int i=0;i<cplistt.size();i++){
-			bw2.write(cplistt.get(i).getTag()+"\t"+cplistt.get(i).getHotIndex()+"\n");
-			bw2.flush();
-		}
-		bw2.close();
-		for(int i=0;i<cplistn.size();i++){
-			bw1.write(cplistn.get(i).getTag()+"\t"+cplistn.get(i).getHotIndex()+"\n");
-			bw1.flush();
-		}
-		bw1.close();
-	}
-    public void countMain()throws IOException {
-		sort();
-		totxt();
+    public void countMain() throws IOException{
+    	BufferedWriter bw=new BufferedWriter(new FileWriter("txt/cpHotListRank.txt"));
+    	BufferedWriter bw1=new BufferedWriter(new FileWriter("txt/cpTotListRank.txt"));
+    	BufferedWriter bw2=new BufferedWriter(new FileWriter("txt/cpRank.txt"));
+    	for(int i=0;i<cplist.length;i++){
+    		for(int j=0;j<hotList.length;j++){
+    			if(hotList[j].getTag().contains(cplist[i].getTag())){
+    				cplist[i].addHot(hotList[j].getHotIndex()+hotList[j].getCommentNum()*10);
+    			}
+    		}
+    	}
+    	List<Couple> l=Arrays.asList(cplist);
+    	Collections.sort(l);
+    	for(int i=0;i<l.size();i++){
+    		bw.write(l.get(i).getTag()+"\t"+l.get(i).getHotIndex()+"\n");
+    		bw.flush();
+    	}
+    	bw.close();
+    	
+    	for(int i=0;i<cplist.length;i++){
+    		for(int j=0;j<totalList.length;j++){
+    			if(totalList[j].getTag().contains(cplist[i].getTag())){
+    				cplist[i].addHot(totalList[j].getHotIndex()+totalList[j].getCommentNum()*10);
+    			}
+    		}
+    	}
+    	l=Arrays.asList(cplist);
+    	Collections.sort(l);
+    	for(int i=0;i<l.size();i++){
+    		bw2.write(l.get(i).getTag()+"\t"+l.get(i).getHotIndex()+"\n");
+    		bw2.flush();
+    	}
+    	bw2.close();
+    	
+    	resetCpIndex();
+    	
+    	for(int i=0;i<cplist.length;i++){
+    		for(int j=0;j<totalList.length;j++){
+    			if(totalList[j].getTag().contains(cplist[i].getTag())){
+    				cplist[i].addHot(totalList[j].getHotIndex()+totalList[j].getCommentNum()*10);
+    			}
+    		}
+    	}
+    	l=Arrays.asList(cplist);
+    	Collections.sort(l);
+    	for(int i=0;i<l.size();i++){
+    		bw1.write(l.get(i).getTag()+"\t"+l.get(i).getHotIndex()+"\n");
+    		bw1.flush();
+    	}
+    	bw1.close();
+    	
     }
+    
     
 }
