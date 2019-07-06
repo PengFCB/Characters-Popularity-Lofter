168289dbf47fb891d37da945e758d10c4e581d27
PengFCB <1159695393@qq.com>
2019-07-06 20:20:11 +0800

    bug fix in Copyright.java

--- rc/1.1/src/team/cplgroup/cpl/Copyright.java
+++ rc/1.1/src/team/cplgroup/cpl/Copyright.java
@@ -24,7 +24,7 @@ public class Copyright extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        //setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
